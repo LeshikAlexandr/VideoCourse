7ab5d18ee5f9b9bb32f00122676330bca0f84d8b
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2022-01-27 20:21:36 +0300

    Doing homework lesson 2

---
+++ src/lesson2/HomeWork.java
@@ -0,0 +1,86 @@
+package lesson2;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+        byte b1 = 12;
+        byte b2 = 0b1100;
+        byte b3 = 014;
+        byte b4 = 0xC;
+
+        System.out.println("b1 = " + b1);
+        System.out.println("b2 = " + b2);
+        System.out.println("b3 = " + b3);
+        System.out.println("b4 = " + b4);
+
+        System.out.println();
+
+        short s1 = 1200;
+        short s2 = 0b10010110000;
+        short s3 = 02260;
+        short s4 = 0x4B0;
+
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+        System.out.println("s3 = " + s3);
+        System.out.println("s4 = " + s4);
+
+        int i1 = 0;
+        int i2 = 0b0;
+        int i3 = 00;
+        int i4 = 0x0;
+
+        System.out.println();
+
+        System.out.println("i1 = " + i1);
+        System.out.println("i2 = " + i2);
+        System.out.println("i3 = " + i3);
+        System.out.println("i4 = " + i4);
+
+        System.out.println();
+
+        long l1 = 123456789L;
+        long l2 = 0b111010110111100110100010101L;
+        long l3 = 0726746425L;
+        long l4 = 0x75BCD15L;
+
+        System.out.println("l1 = " + l1);
+        System.out.println("l2 = " + l2);
+        System.out.println("l3 = " + l3);
+        System.out.println("l4 = " + l4);
+
+        System.out.println();
+
+        float f1 = 2.34f;
+        float f2 = 10;
+
+        System.out.println("f1 = " + f1);
+        System.out.println("f2 = " + f2);
+
+        System.out.println();
+
+        double d1 = 2.34;
+        double d2 = 10;
+
+        System.out.println("d1 = " + d1);
+        System.out.println("d2 = " + d2);
+
+        System.out.println();
+
+        boolean boolean1 = true;
+        boolean boolean2 = false;
+
+        System.out.println("boolean1 = " + boolean2);
+        System.out.println("boolean2 = " + boolean1);
+
+        System.out.println();
+
+        char c1 = 'A';
+        char c2 = 65;
+        char c3 = '\u0041';
+
+        System.out.println("c1 = " + c1);
+        System.out.println("c2 = " + c2);
+        System.out.println("c3 = " + c3);
+    }
+}
