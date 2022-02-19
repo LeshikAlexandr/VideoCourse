b31e2a469e5cfca0cce6d1b21d7efaec36fd167c
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2022-02-19 14:07:17 +0300

    Doing homework for lesson 3

---
+++ src/lesson3/HomeWork.java
@@ -0,0 +1,22 @@
+package lesson3;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        int i1 = 5;
+        int i2 = 11;
+        double d1 = 5.5;
+        double d2 = 1.3;
+
+        long l = 20L;
+        double result = 0;
+        result = i2 / d1 + d2 % i1 - l;
+        System.out.println("result = " + result);
+
+        int a = 5;
+        a = a-- - --a + ++a + a++ + a;
+        int b = 8;
+        b = ++b - b++ + ++b - --b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
